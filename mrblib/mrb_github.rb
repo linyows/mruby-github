1123774b27744ea8258862248f4d8f9427e4b163
linyows <linyows@gmail.com>
2016-12-12 15:57:37 +0900

    fix for client

--- mrblib/mrb_github.rb
+++ mrblib/mrb_github.rb
@@ -3,9 +3,11 @@ class Github
     DEFAULT_API_ENDPOINT = 'https://api.github.com'
 
     attr_writer :token
+    attr_reader :last_response
     attr_accessor :api_endpoint
 
     def initialize(endpoint = nil)
+      @last_response = nil
       @http = HttpRequest.new
       @api_endpoint = endpoint || DEFAULT_API_ENDPOINT
     end
@@ -22,9 +24,10 @@ class Github
     end
 
     def basic_authenticated?(user, password)
-      key = Base64::encode("#{user}:#{password}")
-      res = get('/user', { 'Authorization' => "Basic #{key}" })
-      res.code == 200
+      get('/user', {
+        "Authorization" => "Basic #{Base64::encode("#{user}:#{password}")}"
+      })
+      @last_response.code == 200
     end
 
     def org_teams(org, options = {})
@@ -36,16 +39,30 @@ class Github
     end
 
     def team_member?(team_id, user, options = {})
-      boolean_from_response get("teams/#{team_id}/members/#{user}", options)
+      boolean_from_response(:get, "/teams/#{team_id}/members/#{user}", nil, options)
     end
 
     def get(path, options = {})
-      url = @api_endpoint + path
-      @http.get(url, nil, DEFAULT_HEADERS.merge(options))
+      request(:get, path, nil, options)
     end
 
-    def boolean_from_respoonse(res)
-      res.code == 204
+    def post(path, data, options = {})
+      request(:post, path, data, options)
+    end
+
+    def put(path, data, options = {})
+      request(:put, path, data, options)
+    end
+
+    def request(method, path, data, options = {})
+      url = @api_endpoint + path
+      @last_response = res = @http.send(method, url, data, default_headers.merge(options))
+      JSON::parse(res.body) unless res.body.empty?
+    end
+
+    def boolean_from_response(method, path, data, options = {})
+      request(method, path, data, options)
+      @last_response.code == 204
     end
   end
 end
