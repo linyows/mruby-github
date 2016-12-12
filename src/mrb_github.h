e355d4f145a071020f0beed719b0b3210446568c
linyows <linyows@gmail.com>
2016-12-12 12:23:23 +0900

    rename

---
+++ src/mrb_github.h
@@ -0,0 +1,12 @@
+/*
+** mrb_github.h - Github class
+**
+** See Copyright Notice in LICENSE
+*/
+
+#ifndef MRB_GITHUB_H
+#define MRB_GITHUB_H
+
+void mrb_mruby_github_gem_init(mrb_state *mrb);
+
+#endif
