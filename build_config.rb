aba70e405e21483102b401abf2f92a274421fd25
linyows <linyows@gmail.com>
2016-12-12 15:57:23 +0900

    remove darwin from build_config

--- build_config.rb
+++ build_config.rb
@@ -9,33 +9,6 @@ def gem_config(conf)
 end
 
 MRuby::Build.new do |conf|
-  toolchain :clang
-
-  conf.enable_bintest
-  conf.enable_debug
-  conf.enable_test
-
-  gem_config(conf)
-end
-
-MRuby::Build.new('x86_64-pc-linux-gnu') do |conf|
   toolchain :gcc
-
-  gem_config(conf)
-end
-
-MRuby::CrossBuild.new('x86_64-apple-darwin14') do |conf|
-  toolchain :clang
-
-  [conf.cc, conf.linker].each do |cc|
-    cc.command = 'x86_64-apple-darwin14-clang'
-  end
-
-  conf.cxx.command      = 'x86_64-apple-darwin14-clang++'
-  conf.archiver.command = 'x86_64-apple-darwin14-ar'
-
-  conf.build_target     = 'x86_64-pc-linux-gnu'
-  conf.host_target      = 'x86_64-apple-darwin14'
-
   gem_config(conf)
 end
